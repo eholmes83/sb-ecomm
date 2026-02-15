a7d5f0b39d47006688c2cebed869b09ce4058d63
eholmes83 <eholmes83@yahoo.com>
2026-02-15 16:01:09 -0500

    adding/using lombok

--- src/main/java/com/echapps/ecom/project/category/model/Category.java
+++ src/main/java/com/echapps/ecom/project/category/model/Category.java
@@ -4,21 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-
-    public Category() {}
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
 
     public Long getCategoryId() {
         return categoryId;
