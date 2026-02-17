76b62fa92ccd041bda39d28ecb8b959e59289423
eholmes83 <eholmes83@yahoo.com>
2026-02-17 15:18:53 -0500

    added api response standard

---
+++ src/main/java/com/echapps/ecom/project/category/dto/response/APIResponse.java
@@ -0,0 +1,13 @@
+package com.echapps.ecom.project.category.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    public String message;
+    private boolean isSuccess;
+}
