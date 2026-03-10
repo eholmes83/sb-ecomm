3bb8ecc863d29a17a342d4ac2977ce7cd934f3f6
eholmes83 <eholmes83@yahoo.com>
2026-03-10 15:59:18 -0400

    creating CartItem and Cart repos

--- src/main/java/com/echapps/ecom/project/cart/repository/CartRepository.java
+++ src/main/java/com/echapps/ecom/project/cart/repository/CartRepository.java
@@ -2,8 +2,11 @@ package com.echapps.ecom.project.cart.repository;
 
 import com.echapps.ecom.project.cart.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
+    Cart findCartByEmail(String email);
 }
